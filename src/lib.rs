6c8a046f4f8d14d435018f6cb40e4793e508aa55
josephalfredo-okta <joseph.alfredo@okta.com>
2019-04-30 13:03:40 -0700

    move to lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 
 pub struct Markov<'a> {
   words: Vec<&'a str>,
-  chains: HashMap<&'a str, Vec<&'a str>>,
+  chains: HashMap<(&'a str, &'a str), Vec<&'a str>>,
 }
 
 impl<'a> Markov<'a> {
@@ -16,15 +16,16 @@ impl<'a> Markov<'a> {
   }
 
   pub fn create_sentences(&self, sentence_size: u32) -> String {
-    let mut first_word = self.words[rand::thread_rng().gen_range(0, self.words.len())];
-    let mut sentences = String::from(first_word);
+    let index = rand::thread_rng().gen_range(0, self.words.len() - 1);
+    let mut key = (self.words[index], self.words[index + 1]);
+    let mut sentences = format!("{} {}", key.0, key.1);
 
     while sentences.len() < sentence_size as usize {
-      if let Some(possible_words) = self.chains.get(first_word) {
-        let second_word = possible_words[rand::thread_rng().gen_range(0, possible_words.len())];
+      if let Some(possible_words) = self.chains.get(&key) {
+        let new_key = possible_words[rand::thread_rng().gen_range(0, possible_words.len())];
         sentences.push_str(" ");
-        sentences.push_str(second_word);
-        first_word = second_word;
+        sentences.push_str(new_key);
+        key = (key.1, new_key);
       }
     }
     println!("Creating paragraph with {} word(s)", sentence_size);
@@ -46,18 +47,21 @@ impl<'a> Markov<'a> {
   fn create_chain(&mut self) {
     let n_words = self.words.len();
 
-    for (i, key) in self.words.iter().enumerate() {
-      if n_words > (i + 1) {
-        let word = self.words[i + 1];
-        if !self.chains.contains_key(key) {
-          self.chains.insert(key, vec![word]);
+    for (i, key1) in self.words.iter().enumerate() {
+      if n_words > (i + 2) {
+        let key2 = self.words[i + 1];
+        let word = self.words[i + 2];
+        if !self.chains.contains_key(&(*key1, key2)) {
+          self.chains.insert((key1, key2), vec![word]);
         } else {
-          if let Some(pos) = self.chains.get_mut(key) {
+          if let Some(pos) = self.chains.get_mut(&(*key1, key2)) {
             pos.push(word);
+            // println!("{:?}", self.chains.get(&(*key1, key2)).expect("s"));
           }
         }
       }
     }
-    println!("Chain size : {} distinct words", self.chains.len());
+
+    println!("Chain size : {} distinct words pairs", self.chains.len());
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,22 @@ mod config;
 use config::Config;
 
 mod file;
-mod markov;
-use markov::Markov;
+mod lib;
+use lib::Markov;
 
 fn main() {
   // Initial program setup
   let args: Vec<String> = env::args().skip(1).collect();
   let config = Config::new(&args).unwrap_or_else(|err| {
     eprintln!("Config Error - {}", err);
+    print_help();
     process::exit(1);
   });
 
   // Load file input
   let file_content = file::parse_file(&config.file_name).unwrap_or_else(|err| {
     eprintln!("File parse Error - {}", err);
+    print_help();
     process::exit(1);
   });
 
@@ -26,4 +28,8 @@ fn main() {
 
   let sentence = markov.create_sentences(config.sentence_size);
   println!("{}", sentence);
+}
+
+fn print_help() {
+  println!("usage: markovchain file_path sentence_size(char)")
 }
